98ec7ba50bb032e51e05555e8a67477aaa61bb03
MCris29 <crismax0629@gmail.com>
2021-06-30 18:20:37 -0500

    Login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA7aruzYLJM_Z4l3HLAtvgXipZbVK-u58E',
+    authDomain: 'ionic-chat-db0f9.firebaseapp.com',
+    databaseURL: 'https://ionic-chat-db0f9-default-rtdb.firebaseio.com',
+    projectId: 'ionic-chat-db0f9',
+    storageBucket: 'ionic-chat-db0f9.appspot.com',
+    messagingSenderId: '951021482229',
+    appId: '1:951021482229:web:460f8e51af39a936a855bc',
+    measurementId: 'G-JFL8DXXVPQ',
+  },
 };
 
 /*
